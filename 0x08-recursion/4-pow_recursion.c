dc040151c735781b74827cb2efd928bbced0f8d7
condabu <charlesmagutu11@gmail.com>
2024-03-01 10:04:36 -0800

    Updated function description

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * pow_recursion - returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  *
  * @x: number to rais
  * @y: power to rais
